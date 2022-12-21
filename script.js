c9c13305db334b52de503dd2b07207467fc1300a
Joselyne Uwingabire <uwingajoselyne@gmail.com>
2022-12-21 14:09:30 +0100

    responsiviness

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.querySelector("#title").addEventListener("click", e => {
    const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
    document.querySelector(".container").style.color = color; 
 });
+
+const header = document.querySelector('.top-header');
+const main = document.querySelector('.main-info');
+
+// Set an initial margin for the main element
+main.style.marginTop = `150px`;
+
+// Add a listener to the header element that updates the margin of the main element when the header's height changes
+window.addEventListener('resize', () => {
+  main.style.marginTop = `${header.offsetHeight + 50}px`;
+});
